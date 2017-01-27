98b4bc32a482c327c9003d549ab14391d61bea3f
arith-y-ono <y.ono@arith-metic.jp>
2017-01-27 18:33:55 +0900

    #5 少し修正

--- public/login.js
+++ public/login.js
@@ -18,25 +18,13 @@ var url = auth.generateAuthUrl({ scope: SCOPE });
 var getAccessToken = function(code) {
   auth.getToken(code, function(err, tokens) {
     if (err) {
-      console.log('Error while trying to retrieve access token', err);
+      console.log('Error', err);
       return;
     }
     auth.credentials = tokens;
     upload();
   });
 };
-var upload = function() {
-  var drive = google.drive({ version: 'v2', auth: auth });
-  drive.files.insert({
-    resource: {
-      title: 'My Document',
-      mimeType: 'text/plain'
-    },
-    media: {
-      mimeType: 'text/plain',
-      body: 'Hello World!'
-    }
-}, console.log);
-};
+
 console.log('Visit the url: ', url);
 rl.question('Enter the code here:', getAccessToken);
