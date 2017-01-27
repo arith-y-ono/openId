ae36f75f38c0f1215034fecc36a5f29b302eed74
arith-y-ono <y.ono@arith-metic.jp>
2017-01-27 18:21:27 +0900

    #5 リダイレクトまで実装

--- public/login.js
+++ public/login.js
@@ -1,43 +1,42 @@
-var crypto = require("crypto");
-var url = require("url");
+var readline = require('readline');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 
+var CLIENT_ID = 'client id',
+    CLIENT_SECRET = 'secret',
+    REDIRECT_URL = 'http://localhost:5000?oauth2callback=1',
+    SCOPE = 'https://www.googleapis.com/auth/admin.directory.user.readonly';
 
-var salt = 'hogehoge';
-  var hash = crypto.createHash('sha256');
-  hash.update(salt + Math.random() + Date())
-  var stateToken = hash.digest('hex');
-  console.log(stateToken);
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+var auth = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
-  var authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?'
-    + 'response_type=code'
-    + '&client_id=594466738208-kbma14k39tqclcnuid0ou8pah009v0vo.apps.googleusercontent.com'
-    + '&redirect_uri=http://localhost:5000/oauth2callback=1'
-    + '&scope=profile%20email'
-    + '&state=' + stateToken;
-
-    //リダイレクト処理
-    req.redirect("authUrl");
-
-    var params = url.parse(req.url, true);
-    var code = params.query.code;
-    var state = params.query.state;
-
-    if (state !== session.stateToken) {
-      // 認証に失敗。エラーを返すコードを書く
+var url = auth.generateAuthUrl({ scope: SCOPE });
+var getAccessToken = function(code) {
+  auth.getToken(code, function(err, tokens) {
+    if (err) {
+      console.log('Error while trying to retrieve access token', err);
+      return;
     }
-
-
-    var oauth2 = new oauth.OAuth2(
-    '594466738208-kbma14k39tqclcnuid0ou8pah009v0vo.apps.googleusercontent.com', //cliendId
-   '0lJNwx_ruDS6ceBhasn-o884', //clientSecret
-   'https://accounts.google.com/o/', //baseSite
-   null, //authorizePath
-   'oauth2/token', //accessTokenPath
-   null  //customHeaders
- );
- var param = {
-   grant_type: 'authorization_code',
-   redirect_uri:'http://localhost:5000/oauth2callback=1'
- };
- oauth2.getOAuthAccessToken(code, param, tokenCallback);
+    auth.credentials = tokens;
+    upload();
+  });
+};
+var upload = function() {
+  var drive = google.drive({ version: 'v2', auth: auth });
+  drive.files.insert({
+    resource: {
+      title: 'My Document',
+      mimeType: 'text/plain'
+    },
+    media: {
+      mimeType: 'text/plain',
+      body: 'Hello World!'
+    }
+}, console.log);
+};
+console.log('Visit the url: ', url);
+rl.question('Enter the code here:', getAccessToken);
